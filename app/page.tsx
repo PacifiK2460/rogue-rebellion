0a80afe08283c1691d9d88cfdc6582b08834083e
Santiago de la cruz Martinez Lara <chago1929@hotmail.com>
2024-04-02 17:38:59 -0600

    Better Hero Page

--- app/page.tsx
+++ app/page.tsx
@@ -6,160 +6,68 @@ import { TracingBeam } from "@/components/ui/tracing-beam";
 import { AnimatePresence, motion } from "framer-motion";
 import { CanvasRevealEffect } from "@/components/ui/canvas-reveal-effect";
 
-// const Card = ({
-//   title,
-//   // icon,
-//   children,
-// }: {
-//   title: string;
-//   // icon: React.ReactNode;
-//   children?: React.ReactNode;
-// }) => {
-//   const [hovered, setHovered] = React.useState(false);
-//   return (
-//     <div
-//       onMouseEnter={() => setHovered(true)}
-//       onMouseLeave={() => setHovered(false)}
-//       className="border border-black/[0.2] group/canvas-card flex items-center justify-center dark:border-white/[0.2]  max-w-sm w-full mx-auto p-4 relative h-[30rem]"
-//     >
-//       <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
-//       <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
-//       <Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
-//       <Icon className="absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black" />
+import { useState } from 'react'
+import { Dialog } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
-//       <AnimatePresence>
-//         {hovered && (
-//           <motion.div
-//             initial={{ opacity: 0 }}
-//             animate={{ opacity: 1 }}
-//             className="h-full w-full absolute inset-0"
-//           >
-//             {children}
-//           </motion.div>
-//         )}
-//       </AnimatePresence>
+const navigation = [
+  { name: 'Product', href: '#' },
+  { name: 'Features', href: '#' },
+  { name: 'Marketplace', href: '#' },
+  { name: 'Company', href: '#' },
+]
 
-//       <div className="relative z-20">
-//         <div className="text-center group-hover/canvas-card:-translate-y-4 group-hover/canvas-card:opacity-0 transition duration-200 w-full  mx-auto flex items-center justify-center">
-//           {/* {icon} */}
-//         </div>
-//         <h2 className="dark:text-white text-xl opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 transition duration-200">
-//           {title}
-//         </h2>
-//       </div>
-//     </div>
-//   );
-// };
-
-// const AceternityIcon = () => {
-//   return (
-//     <svg
-//       width="66"
-//       height="65"
-//       viewBox="0 0 66 65"
-//       fill="none"
-//       xmlns="http://www.w3.org/2000/svg"
-//       className="h-10 w-10 text-black dark:text-white group-hover/canvas-card:text-white "
-//     >
-//       <path
-//         d="M8 8.05571C8 8.05571 54.9009 18.1782 57.8687 30.062C60.8365 41.9458 9.05432 57.4696 9.05432 57.4696"
-//         stroke="currentColor"
-//         strokeWidth="15"
-//         strokeMiterlimit="3.86874"
-//         strokeLinecap="round"
-//         style={{ mixBlendMode: "darken" }}
-//       />
-//     </svg>
-//   );
-// };
-
-// export const Icon = ({ className, ...rest }: any) => {
-//   return (
-//     <svg
-//       xmlns="http://www.w3.org/2000/svg"
-//       fill="none"
-//       viewBox="0 0 24 24"
-//       strokeWidth="1.5"
-//       stroke="currentColor"
-//       className={className}
-//       {...rest}
-//     >
-//       <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m6-6H6" />
-//     </svg>
-//   );
-// };
 
 export default function Home() {
-
-  const ref = React.useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ["start start", "end start"],
-  });
-
-  const pathLengthFirst = useTransform(scrollYProgress, [0, 0.8], [0.2, 1.2]);
-  const pathLengthSecond = useTransform(scrollYProgress, [0, 0.8], [0.15, 1.2]);
-  const pathLengthThird = useTransform(scrollYProgress, [0, 0.8], [0.1, 1.2]);
-  const pathLengthFourth = useTransform(scrollYProgress, [0, 0.8], [0.05, 1.2]);
-  const pathLengthFifth = useTransform(scrollYProgress, [0, 0.8], [0, 1.2]);
-
-  const [hovered, setHovered] = React.useState(false);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
     <main >
-      <div className="h-[350vh] w-full rounded-md relative pt-40 overflow-clip">
-        <GoogleGeminiEffect pathLengths={[
-          pathLengthFirst,
-          pathLengthSecond,
-          pathLengthThird,
-          pathLengthFourth,
-          pathLengthFifth,
-        ]} title="Rogue Rebellion" description="Desafía lo convencional — viste la rebelión" className="" />
-      </div>
-
-      <div
-        onMouseEnter={() => setHovered(true)}
-        onMouseLeave={() => setHovered(false)}
-        className="h-[40rem] flex flex-col lg:flex-col overflow-hidden items-center justify-center bg-black w-full gap-4 mx-auto px-8 relative"
-      >
-        <p className="md:text-2xl text-2xl font-medium text-center text-white relative z-20 max-w-2xl mx-auto">
-          Rogue Rebellion se refiere a prendas confeccionadas con tejidos técnicos como GORE-TEX, nailon Primaloft y forro polar Polartec, diseñadas para permitir resistencia al agua, transpirabilidad, resistencia al viento y comodidad.
-        </p>
-
-        <p className="md:text-2xl text-2xl font-medium text-center text-white relative z-20 max-w-2xl mx-auto">
-          Rogue Rebellion también puede describir una estética específica inspirada en la cultura cyberpunk y la moda urbana.
-        </p>
-        <AnimatePresence>
-          {hovered && (
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              className="h-full w-full absolute inset-0"
-            >
-              <CanvasRevealEffect
-                animationSpeed={5}
-                containerClassName="bg-transparent"
-                colors={[
-                  [59, 130, 246],
-                  [139, 92, 246],
-                ]}
-                opacities={[0.2, 0.2, 0.2, 0.2, 0.2, 0.4, 0.4, 0.4, 0.4, 1]}
-                dotSize={2}
-              />
-            </motion.div>
-          )}
-        </AnimatePresence>
-        {/* Radial gradient for the cute fade */}
-        <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" />
-      </div>
-
-      <div>
-        <>
-          <div className="py-20 flex flex-col lg:flex-row items-center justify-center bg-white dark:bg-black w-full gap-4 mx-auto px-8">
-
+      <div className="bg-black">
+        <div className="relative isolate px-6 pt-14 lg:px-8">
+          <div
+            className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+            aria-hidden="true"
+          >
+            <div
+              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              }}
+            />
           </div>
-        </>
+          <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+            <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+              <div className="relative rounded-full px-3 py-1 text-md leading-6 text-gray-300 ring-1 ring-gray-100/10 hover:ring-gray-100/20">
+                Vive el Hype. Sé el Hype. Únete a la Compañía.{' '}
+                <a href="#" className="font-semibold text-indigo-600">
+                  <span className="absolute inset-0" aria-hidden="true" />
+                  Unete <span aria-hidden="true">&rarr;</span>
+                </a>
+              </div>
+            </div>
+            <div className="text-center">
+              <h1 className="text-6xl font-bold tracking-tight text-gray-100 sm:text-6xl">
+                The Hype Company
+              </h1>
+              <p className="mt-6 text-xl leading-8 text-gray-300">
+                Descubre colecciones que desafían las normas, diseñadas para aquellos que se atreven a destacar. </p>
+            </div>
+          </div>
+          <div
+            className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+            aria-hidden="true"
+          >
+            <div
+              className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              }}
+            />
+          </div>
+        </div>
       </div>
     </main>
   );
